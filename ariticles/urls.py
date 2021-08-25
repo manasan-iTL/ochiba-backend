61c0ae8e0630a6fd9633b5f2ca98859cc26184c1
Shota Yamamoto <kuaiwanga@gamil.com>
2021-08-25 21:59:20 +0900

    first commit by山本

---
+++ ariticles/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+
+
+app_name = 'diary'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name="index"),
+    path('article-list/', views.ArticleListView.as_view(), name="article_list"),
+    path('article-detail/<int:pk>/', views.ArticleDetailView.as_view(), name="article_detail"),
+    path('article-create/', views.ArticleCreateView.as_view(), name="diary_create"),
+    path('article-update/<int:pk>/', views.ArticleUpdateView.as_view(), name="article_update"),
+    path('article-delete/<int:pk>/', views.ArticleDeleteView.as_view(), name="article_delete"),
+    path('<slug:username>', views.ProfileDetailView.as_view(), name='profile'),
+    path('<slug:username>/edit', views.ProfileUpdateView.as_view(), name='edit'),
+]
