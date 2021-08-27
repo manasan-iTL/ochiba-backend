4dc1b78c9c77313101713867c8a279fdaa20ab41
manasan-iTL <daiyaofssh2020@gmail.com>
2021-08-28 01:28:26 +0900

    articlesのmodel修正

--- articles/models.py
+++ articles/models.py
@@ -4,20 +4,22 @@ from django.db.models.deletion import CASCADE, SET_NULL
 from django.db.models.fields import CharField
 # Create your models here.
 
-class Object(models.Model):
-    url = models.URLField()
-    title = models.CharField("タイトル", max_length=100, blank=False, null=False)
-    discription = models.TextField("概要", blank=True, null=False)
-
-    def __str__(self):
-        return self.title 
-
 class Post(models.Model):
-    test = models.TextField("テスト")
     title = models.CharField("タイトル", max_length=100, blank=False, null=False)
     discription = models.TextField("概要", blank=True, null=False)
     created_at = models.DateTimeField(auto_now_add=True, editable=False, blank=False, null=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False, blank=False, null=False)
     status = models.BooleanField(default=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    object = models.ManyToManyField("Object")
+
+    def __str__(self):
+        return self.title
+
+class Object(models.Model):
+    url = models.URLField()
+    title = models.CharField("タイトル", max_length=100, blank=False, null=False)
+    discription = models.TextField("概要", blank=True, null=False)
+    post_data = models.ForeignKey(Post, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.title 
