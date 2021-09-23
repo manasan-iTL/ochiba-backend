6c8553a4e23a4ec23a479e2e24878e74cb29f131
manasan-iTL <daiyaofssh2020@gmail.com>
2021-09-23 19:30:26 +0900

    新規作成・編集のバック完了

--- articles/urls.py
+++ articles/urls.py
@@ -6,9 +6,12 @@ from articles import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
-    path('article-list/', views.ArticleListView.as_view(), name="article_list"),
     path('article-detail/<int:pk>/', views.ArticleDetailView.as_view(), name="article_detail"),
-    path('article-create/', views.ArticleCreateView.as_view(), name="diary_create"),
-    path('article-update/<int:pk>/', views.ArticleUpdateView.as_view(), name="article_update"),
-    path('article-delete/<int:pk>/', views.ArticleDeleteView.as_view(), name="article_delete"),
+    path('create-post/', views.CreatePostView.as_view(), name="create_post"),
+    path('create-object/', views.CreateObjectView.as_view(), name="create_object"),
+    path('test/', views.SampleCreateObjectView.as_view(), name="test"),
+    path('article-post/<int:pk>/newupdate', views.SampleUpdateObjectView.as_view(), name="object_update"),
+    path('article-delete/<int:pk>/delete', views.ArticleDeleteView.as_view(), name="article_delete"),
+    path('article-post/<int:pk>/update', views.ArticleUpdateView.as_view(), name="article_update"),
+    path('sample/', views.CreateObjectPostView.as_view(), name="test2"),
 ]
