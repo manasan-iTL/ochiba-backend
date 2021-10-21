61d62e9c4697a39d20a0b39bf063bdb471ea0c6d
阿部侑生 <abeyuuki@abeyuuseinoMacBook-Pro.local>
2021-10-21 20:53:23 +0900

    コミット

--- BookmarkShareApp/settings.py
+++ BookmarkShareApp/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'BookmarkShareApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bookmarkshares',
-        'USER': 'postgres',
-        'PASSWORD': '1531',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
