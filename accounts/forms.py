f11b8da573432d7e8a2a875512b0bd52391bc2f8
manasan-iTL <daiyaofssh2020@gmail.com>
2021-12-28 17:55:08 +0900

    [change]usernameを自分で設定できるようにした。

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,17 +1,27 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.core import validators
+from django.core.validators import RegexValidator, slug_re
+from django.db.models.base import Model
 from allauth.account.forms import SignupForm
 from django.forms.fields import CharField
 from django.http import request
 from .models import CustomUser
 
 class ProfileEditForm(forms.Form):
-    # username = forms.CharField(max_length=30, label='ユーザー名')
+    username = forms.CharField(max_length=30, 
+    label='ユーザー名', 
+    validators=[RegexValidator(slug_re,'名前には半角英数字、アンダーバー、ハイフンのみ指定できます。')],
+    widget=forms.TextInput(attrs={'pattern':'^[A-Za-z0-9_-]+$'}))
     # password = forms.CharField(max_length=30, label='パスワード') 出したらエラーが出る
     first_name = forms.CharField(max_length=30, label='姓', required=False)
     last_name = forms.CharField(max_length=30, label='名', required=False)
     description = forms.CharField(label='自己紹介', widget=forms.Textarea(), required=False)
     image = forms.ImageField(required=False)
+
+# class ProfileEditForm(forms.ModelForm):
+#   class Meta:
+#     model = CustomUser
 
 
 class SignupForm(SignupForm):
