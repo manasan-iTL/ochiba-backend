9967adcf49ee31ef246438fd353a93b8f44e1edf
manasan-iTL <daiyaofssh2020@gmail.com>
2022-04-11 10:35:24 +0900

    [delete] log出力を全部消した

--- assets/js/loginValidation.js
+++ assets/js/loginValidation.js
@@ -1,4 +1,3 @@
-console.log('Click');
 let element = document.querySelectorAll('.is-validate');
 let errorlists = document.querySelectorAll('.errorlist');
 const email = document.getElementById('id_login');
@@ -6,13 +5,10 @@ const pass = document.getElementById('id_password');
 const pass1 = document.getElementById('id_password1');
 const pass2 = document.getElementById('id_password2');
 
-console.log(errorlists);
-
 
 
 for(let i=0; i < element.length; i++) {
     if(element[i].innerHTML !== "") {
-        console.log('ok');
         email.classList.add('validate-error');
         pass.classList.add('validate-error');
         element[i].classList.add('error-text');
@@ -21,7 +17,6 @@ for(let i=0; i < element.length; i++) {
 
 for(let k=0; k < errorlists.length; k++) {
     if(errorlists[k].innerHTML !== "") {
-        console.log('ok');
         email.classList.add('validate-error');
         pass1.classList.add('validate-error')
         pass2.classList.add('validate-error')
