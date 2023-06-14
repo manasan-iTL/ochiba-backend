534222b7e85a08ef013ea5809e52317a9560b552
manasan-iTL <daiyaofssh2020@gmail.com>
2023-06-14 23:59:34 +0900

    add Object用のSerializerを作成

--- backend/articles/serializers.py
+++ backend/articles/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Object
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ["id", "title", "discription", "status"]
+        fields = ["id", "title", "discription", "status", "created_at", "updated_at", "user"]
+
+class ObjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Object
+        fields = ["id", "title", "url", "discription", "post_data"]
